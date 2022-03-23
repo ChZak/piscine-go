546d0e3f973d86f500578d4fc6d9ef2e02ec5da1
zakch <zakaria.chaikhi@gmail.com>
2022-03-23 13:56:36 +0100

    okokkkok

--- isnegative.go
+++ isnegative.go
@@ -5,13 +5,9 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
+		z01.PrintRune('\n')
 	} else {
 		z01.PrintRune('F')
+		z01.PrintRune('\n')
 	}
-}
-
-func main() {
-	IsNegative(0)
-	IsNegative(1)
-	IsNegative(-1)
 }
