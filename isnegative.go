8cfaa85a3e525f2ce4bd383857648bd1c065c2d7
zakch <zakaria.chaikhi@gmail.com>
2022-03-23 13:38:24 +0100

    bahoui

--- isnegative.go
+++ isnegative.go
@@ -3,9 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	if nb > 0 {
-		z01.PrintRune('F')
-	} else {
+	if nb < 0 {
 		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
 	}
 }
