421dddb7550f27973efea0d30fb17f44479bb942
zakch <zakaria.chaikhi@gmail.com>
2022-03-24 13:33:22 +0100

    kllllllllll

--- printstr.go
+++ printstr.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
 	r := []rune(s)
-	for _, r := range r {
-		z01.PrintRune(r)
+	for _, res := range r {
+		z01.PrintRune(res)
 	}
 	z01.PrintRune('\n')
 }
