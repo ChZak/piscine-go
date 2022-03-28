dae909501f0f7860fe65026acccfdbb209a80a3a
zakch <zakaria.chaikhi@gmail.com>
2022-03-28 13:13:46 +0200

    ookokook

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,14 +2,14 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	f := 1
-	if nb > 0 {
+	if nb < 0 {
+		return 0
+	} else if nb == 1 || nb == 0 {
+		return 1
+	} else {
 		for i := 1; i <= nb; i++ {
 			f = f * i
 		}
-	} else if nb == 0 || nb == 1 {
-		return 1
-	} else {
-		return 0
 	}
 	return f
 }
