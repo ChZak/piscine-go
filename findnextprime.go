5448b4ffc7f451b61f598081a6ea5973983a67fc
zakch <zakaria.chaikhi@gmail.com>
2022-03-28 19:47:52 +0200

    okokokokko

--- findnextprime.go
+++ findnextprime.go
@@ -1,23 +1,13 @@
 package piscine
 
-func IsPrime(nb int) bool {
-	if nb <= 1 {
-		return false
-	}
-	for i := 2; i <= nb/2; i++ { // si il y a un chiffre apres la virgule lors de la division, c'est un nombre premier
-		if nb%i == 0 { // si le chiffre apres la virgule est zero apres la division, ce n'est pas un nomnbre premier
-			return false
-		}
-	}
-	return true
-}
+import "piscine"
 
 func FindNextPrime(nb int) int {
-	if IsPrime(nb) == true {
+	if piscine.IsPrime(nb) == true {
 		return nb
 	} else {
 		n := nb + 1
-		for IsPrime(n) == false {
+		for piscine.IsPrime(n) == false {
 			n++
 		}
 		return n
