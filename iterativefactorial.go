3c8324ca467502120199e9ebca60619020f21c07
zakch <zakaria.chaikhi@gmail.com>
2022-03-28 13:19:42 +0200

    essaie

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	f := 1
 	if nb < 0 {
 		return 0
 	} else if nb == 1 || nb == 0 {
 		return 1
 	} else {
-		for i := 1; i <= nb; i++ {
+		f := 1
+		for i := 1; i < nb+1; i++ {
 			f = f * i
 		}
 		return f
