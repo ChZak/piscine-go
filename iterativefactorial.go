1a25d2030b2e48568161f2bd61f46c6380a6b07f
zakch <zakaria.chaikhi@gmail.com>
2022-03-28 13:45:55 +0200

    okokokokokokokokok

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,21 +1,19 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb > 0 {
-		result := 1
+	if nb < 0 {
+		return 0
+	} else if nb == 0 || nb == 1 {
+		return 1
+	} else {
+		res := 1
 		for i := 1; i <= nb; i++ {
-			result *= i
-			if result > 2147483647 {
-				return 0
+			res *= i
+			if res > 2147483647 {
+				res = 0
+				break
 			}
 		}
-		return result
-
-	} else if nb == 1 || nb == 0 {
-		return 1
-	} else if nb < 0 {
-		return 0
-	} else {
-		return 0
+		return res
 	}
 }
