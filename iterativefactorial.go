542e0237b33457e7d514a8324409be3968605905
zakch <zakaria.chaikhi@gmail.com>
2022-03-28 14:40:41 +0200

    okok

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,14 +5,12 @@ func IterativeFactorial(nb int) int {
 		return 0
 	} else if nb == 0 || nb == 1 {
 		return 1
-	} else {
-		res := 1
-		for i := 1; i <= nb; i++ {
-			res *= i
-			if res >= 2147483647 {
-				return 0
-			}
-		}
-		return res
+	} else if nb >= 2147483647 {
+		return 0
 	}
+	res := 1
+	for i := 1; i <= nb; i++ {
+		res *= i
+	}
+	return res
 }
