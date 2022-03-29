ce52ba3a13b11e46952e562cb04db2de1d7918ce
zakch <zakaria.chaikhi@gmail.com>
2022-03-29 15:46:58 +0200

    okokokok

--- alphacount.go
+++ alphacount.go
@@ -4,13 +4,11 @@ func AlphaCount(s string) int {
 	myS := []rune(s)
 	count := 1
 	for i := 0; i < StrLen(s); i++ {
-		if myS[i] >= 41 && myS[i] <= 90 {
+		if myS[i] >= 65 && myS[i] <= 90 {
 			count += 1
 		} else if myS[i] >= 97 && myS[i] <= 122 {
 			count += 1
-		} else {
-			count -= 1
 		}
 	}
-	return count
+	return count - 1
 }
