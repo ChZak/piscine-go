5d3ff83e94254cc68f95018f51c99449aacf0033
zakch <zakaria.chaikhi@gmail.com>
2022-03-29 12:20:51 +0200

    test

--- nrune.go
+++ nrune.go
@@ -4,7 +4,7 @@ func NRune(s string, n int) rune {
 	r := []rune(s)
 	if n > 0 && n <= StrLen(s) {
 		for i := 0; StrLen(s) != n+1; i++ {
-			if i == n {
+			if i == n+1 {
 				return r[n-1]
 			}
 		}
