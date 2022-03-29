0fa6ee1bf6453d349f348fba396ba712651d869f
zakch <zakaria.chaikhi@gmail.com>
2022-03-29 12:12:46 +0200

    okoko

---
+++ nrune.go
@@ -0,0 +1,13 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	r := []rune(s)
+	if n > 0 && n <= StrLen(s) {
+		for i := 0; StrLen(s) != n+1; i++ {
+			if i == n {
+				return r[n-1]
+			}
+		}
+	}
+	return 0
+}
