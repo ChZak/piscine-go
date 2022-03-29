edec6eba0eb19dd1dfa740b541be52d94ecffb09
zakch <zakaria.chaikhi@gmail.com>
2022-03-29 20:42:44 +0200

    okokokokokokokokok

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	str := []rune(s)
 	for r := 0; r < len(str); r++ {
-		if str[r] < 33 || str[r] > 126 {
+		if str[r] < 32 || str[r] > 126 {
 			return false
 		}
 	}
