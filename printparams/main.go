7c783157d90c6d08cb014246cb2a9c40055b3a0d
zakch <zakaria.chaikhi@gmail.com>
2022-03-31 16:41:26 +0200

    bawe

---
+++ printparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	for i := 1; i < len(args); i++ {
+		runes := []rune(args[i])
+		for j := 0; j < len(runes); j++ {
+			if runes[j] > '0' {
+				z01.PrintRune(runes[j])
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
