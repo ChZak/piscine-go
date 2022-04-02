507310a0018191602f9bb2613323419a62a42aaa
zakch <zakaria.chaikhi@gmail.com>
2022-04-02 14:20:28 +0200

    test

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,10 +1,20 @@
 package piscine
 
-import (
-	"strings"
-)
-
 func SplitWhiteSpaces(s string) []string {
-	myArray := []string(strings.Fields(s))
-	return myArray
+	myStr := ""
+	myTab := []string{}
+	for i, char := range s {
+		if i == len(s)-1 && string(char) != " " { // si il y a pas despace
+			myStr += string(char)        // ajoute les charactere a ma nouvelle string
+			myTab = append(myTab, myStr) // et ajoute la string a mon tableau
+		} else if string(char) != " " && string(char) != "\n" { // sinon si on est pas a la fin de la chaine et qu'on a pas rencontrer d'espace
+			myStr += string(char) // Ajoute les characetre a ma string
+		} else {
+			if len(myStr) >= 1 { // si il y a un ou pleusiur mot
+				myTab = append(myTab, myStr) // ajouter les a mon tableau
+			}
+			myStr = "" // remet la chaine a zero pour chaque mot
+		}
+	}
+	return myTab
 }
