a393bde98445e64eed4640745c418f093b3a0d86
zakch <zakaria.chaikhi@gmail.com>
2022-04-05 13:27:17 +0200

    okok

---
+++ displayfile/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	length := 0
+
+	for range args {
+		length++
+	}
+	if length > 1 {
+		fmt.Println("Too many arguments")
+	} else if length == 0 {
+		fmt.Println("File name missing")
+	} else if length == 1 {
+		content, err := ioutil.ReadFile(args[0]) // content recois le contenu du fichier, et err une erreur si il y en a une
+		if err != nil {                          // Un appel reussi a readil renvoi err == nil
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(string(content))
+	}
+}
