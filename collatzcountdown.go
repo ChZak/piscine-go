6e9f04e5f2b4fd5200cbef7f552cd6aafcab6882
zakch <zakaria.chaikhi@gmail.com>
2022-04-07 16:49:21 +0200

    okokko

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,6 +1,8 @@
 package piscine
 
 func CollatzCountdown(start int) int {
+	count := 0
+
 	if start <= 0 {
 		return -1
 	}
@@ -8,9 +10,11 @@ func CollatzCountdown(start int) int {
 	for start != 1 {
 		if start%2 == 0 {
 			start /= 2
+			count++
 		} else {
-			start *= 3 + 1
+			start = 3*start + 1
+			count++
 		}
 	}
-	return start
+	return count
 }
