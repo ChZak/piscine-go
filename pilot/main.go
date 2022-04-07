d674906fbad0d656eb14481ae3e6e3b83f87d3b6
zakch <zakaria.chaikhi@gmail.com>
2022-04-07 20:21:07 +0200

    okok

---
+++ pilot/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var donnie struct {
+		Name     string
+		Life     int
+		Age      int
+		Aircraft int
+	}
+
+	donnie.Name = "Donnie"
+	donnie.Life = 100.0
+	donnie.Age = 24
+	donnie.Aircraft = 1
+
+	fmt.Println(donnie)
+}
