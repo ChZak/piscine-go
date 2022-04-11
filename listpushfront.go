f4f9dd56c6149f14fd5b86ac58f979633c2118c5
zakch <zakaria.chaikhi@gmail.com>
2022-04-11 17:58:54 +0200

    okokok

---
+++ listpushfront.go
@@ -0,0 +1,23 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	ele := &NodeL{
+		Data: data,
+	}
+	if l.Head == nil {
+		l.Head = ele
+	} else {
+		ele.Next = l.Head // Ajoute le Head a l'élement suivant
+		l.Head = ele      // Remplace head par lement ajouter
+	}
+}
