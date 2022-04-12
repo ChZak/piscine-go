af3f377f9441b23d180a395dbaba89fc496b5624
zakch <zakaria.chaikhi@gmail.com>
2022-04-12 15:39:19 +0200

    okok

--- listreverse.go
+++ listreverse.go
@@ -4,7 +4,6 @@ func ListReverse(l *List) {
 	currentNode := l.Head
 	var next *NodeL
 	var previousNode *NodeL
-	l.Tail = l.Head
 	for currentNode != nil {
 		next, currentNode.Next = currentNode.Next, previousNode
 
