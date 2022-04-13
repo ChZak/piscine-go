8756f87d1fc7f3bb94bad6670dfb5a05a20d8f90
zakch <zakaria.chaikhi@gmail.com>
2022-04-13 13:55:09 +0200

    okokok

--- listpushback.go
+++ listpushback.go
@@ -11,16 +11,16 @@ type List struct {
 }
 
 func ListPushBack(l *List, data interface{}) {
-	ele := &NodeL{ // Ajoute les data a Data
+	ele := &NodeL{
 		Data: data,
 	}
-	if l.Head == nil { // Si le premier noeud de la liste est vide
-		l.Head = ele // Il ajoute data a Head (premier element de la liste)
+	if l.Head == nil {
+		l.Head = ele
 	} else {
-		current := l.Head         // current contient le premier noeud de la liste
-		for current.Next != nil { // si le noeud suivant n'est pas nil ca veut dire quil y a dautre noeud
-			current = current.Next // Temps qu'on est pas arriver a la fin de la list continue
+		current := l.Head
+		for current.Next != nil {
+			current = current.Next
 		}
-		current.Next = ele // On ajoute 'ele' au noeud suivant
+		current.Next = ele
 	}
 }
