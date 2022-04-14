024cf66e19af5be5a371e64a6d74301069763b7d
zakch <zakaria.chaikhi@gmail.com>
2022-04-14 17:15:14 +0200

    test

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -3,10 +3,10 @@ package piscine
 func BTreeLevelCount(root *TreeNode) int {
 	if root != nil {
 		var result int = BTreeLevelCount(root.Left) + BTreeLevelCount(root.Right)
-		if root.Left != nil && root.Right != nil {
-			return result
-		} else {
+		if root.Left == nil || root.Right == nil {
 			return result + 1
+		} else if root.Left != nil || root.Right != nil {
+			return result
 		}
 	}
 	return 0
